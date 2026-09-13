4f4b25f00b519cb25ea986ee159a8e5633544fa1
Thomasignature <bothythomas1@gmail.com>
2026-09-13 20:19:56 +0200

    Load V2 dashboard styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app.jsx'
 import './styles.css'
+import './v2.css'
 
 createRoot(document.getElementById('root')).render(<App />)
 
